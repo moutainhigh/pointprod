d7470e219d0e716a0ae8d48b26e962c310750fda
梅小虎 <meixiaohu@emoney.cn>
2021-04-14 18:58:46 +0800

    point prod 我的消息

--- emoney-pointweb/src/main/java/com/emoney/pointweb/repository/impl/PointOrderRepositoryImpl.java
+++ emoney-pointweb/src/main/java/com/emoney/pointweb/repository/impl/PointOrderRepositoryImpl.java
@@ -32,7 +32,7 @@ public class PointOrderRepositoryImpl implements PointOrderRepository {
 
     @Override
     public List<PointOrderDO> getByUid(Long uid,Integer orderStatus,int pageIndex,int pageSize) {
-        //PageHelper.startPage(pageIndex,pageSize);
+        PageHelper.startPage(pageIndex,pageSize);
         List<PointOrderDO> list=pointOrderMapper.getByUid(uid,orderStatus);
 
         return null;
