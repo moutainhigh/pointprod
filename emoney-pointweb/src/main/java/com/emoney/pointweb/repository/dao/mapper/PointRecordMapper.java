92ae09522801786631e0c3f5afcc3400cc4cf54d
meixiaohu <meixiaohu@@emoney.cn>
2021-05-11 09:09:02 +0800

    增加注释

--- emoney-pointweb/src/main/java/com/emoney/pointweb/repository/dao/mapper/PointRecordMapper.java
+++ emoney-pointweb/src/main/java/com/emoney/pointweb/repository/dao/mapper/PointRecordMapper.java
@@ -37,19 +37,65 @@ public interface PointRecordMapper {
      */
     List<PointRecordDO> getByPager(Long uid,Integer pointStatus,Date startDate, Date endDate);
 
+    /**
+     * 根据uid查询非每日任务积分记录
+     * @param uid
+     * @return
+     */
     List<PointRecordDO> getByUid1(Long uid);
 
+    /**
+     * 根据uid查询每日任务当日积分记录
+     * @param uid
+     * @param startDate
+     * @param endDate
+     * @return
+     */
     List<PointRecordDO> getByUid2(Long uid, Date startDate, Date endDate);
 
+    /**
+     * 根据uid查询带领取任务记录
+     * @param uid
+     * @return
+     */
     List<PointRecordDO> getUnClaimRecordsByUid(Long uid);
 
+    /**
+     * 修改积分记录
+     * @param pointRecordDO
+     * @return
+     */
     Integer update(PointRecordDO pointRecordDO);
 
+    /**
+     * 根据uid查询积分记录按照状态分组统计
+     * @param uid
+     * @return
+     */
     List<PointRecordSummaryDO> getPointRecordSummaryByUid(Long uid);
 
+    /**
+     * 根据uid,时间范围 查询积分记录按照状态分组统计
+     * @param uid
+     * @param dtStart
+     * @param dtEnd
+     * @return
+     */
     List<PointRecordSummaryDO> getPointRecordSummaryByUidAndCreateTime(Long uid, Date dtStart, Date dtEnd);
 
+    /**
+     * 根据uid，taskIds批量查询任务记录
+     * @param uid
+     * @param taskIds
+     * @return
+     */
     List<PointRecordDO> getPointRecordByTaskIds(Long uid, @Param("list") List<Long> taskIds);
 
+    /**
+     * 根据uid查询当前获得的未使用积分
+     * @param uid
+     * @param endDate
+     * @return
+     */
     List<PointRecordDO> getByUidAndCreateTime(Long uid, Date endDate);
 }
