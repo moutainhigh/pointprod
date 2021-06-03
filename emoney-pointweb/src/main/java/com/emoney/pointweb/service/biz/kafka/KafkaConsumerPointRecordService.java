ffde4019578102bdf93a86691771c89a46888583
meixiaohu <meixiaohu@@emoney.cn>
2021-06-03 20:11:48 +0800

    kafka消费

--- emoney-pointweb/src/main/java/com/emoney/pointweb/service/biz/kafka/KafkaConsumerPointRecordService.java
+++ emoney-pointweb/src/main/java/com/emoney/pointweb/service/biz/kafka/KafkaConsumerPointRecordService.java
@@ -59,7 +59,7 @@ public class KafkaConsumerPointRecordService {
     private ThreadPoolTaskExecutor executor;
 
     // 消费监听
-    @KafkaListener(topics = {"pointrecordadd"})
+    @KafkaListener(topics = {"pointrecordadd"}, groupId = "pointrecordgroup")
     public void onMessage(@Payload ConsumerRecord<?, ?> record, Acknowledgment acknowledgment) {
         try {
             // 消费的哪个topic、partition的消息,打印出消息内容
