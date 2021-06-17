9a2372ff86bd90d97cda2b819a061eca1fd6e06b
meixiaohu <meixiaohu@@emoney.cn>
2021-06-17 19:07:35 +0800

    订单兑换增加权限验证

---
+++ emoney-pointweb/src/main/java/com/emoney/pointweb/repository/dao/entity/dto/CheckWebOrderDTO.java
@@ -0,0 +1,10 @@
+package com.emoney.pointweb.repository.dao.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class CheckWebOrderDTO {
+    private String Account;
+    private String ActivityCode;
+    private String MID;
+}
